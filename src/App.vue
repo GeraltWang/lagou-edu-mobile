33001c7225f2571848715e96225d7b8bbec18f45
GeraltWang <wgeralt@outlook.com>
2021-10-25 00:00:50 +0800

    课程页面制作中

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <!-- 根路由出口 -->
-  <router-view/>
+  <div>
+    <!-- 根路由出口 -->
+    <router-view/>
+  </div>
 </template>
-
+<script>
+import request from '@/utils/request'
+const { data } = request({
+  method: 'GET',
+  url: '/front/ad/getAdList'
+})
+console.log(data)
+export default {
+}
+</script>
 <style lang="scss">
 </style>
---
+++ src/views/course/components/CourseContent.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="course-content">
+      <!-- 轮播图 -->
+      <van-swipe class="my-swipe" lazy-render :autoplay="3000" indicator-color="white">
+        <van-swipe-item v-for="item in activeAdList" :key="item.id">
+            <img :src="item.img">
+            <!-- <van-image :src="item.img"></van-image> -->
+        </van-swipe-item>
+      </van-swipe>
+      <!-- 课程列表 -->
+      <course-content-list></course-content-list>
+  </div>
+</template>
+
+<script>
+import { getAllAds } from '@/services/course'
+import CourseContentList from './CourseContentList.vue'
+
+export default {
+  name: 'CourseContent',
+  components: {
+    CourseContentList
+  },
+  data () {
+    return {
+      // 轮播图
+      adList: []
+    }
+  },
+  created () {
+    // 请求轮播图
+    this.loadAllAds()
+  },
+  methods: {
+    async loadAllAds () {
+      const { data } = await getAllAds({
+        // 999 代表首页顶部轮播图广告位
+        spaceKeys: 999
+      })
+      console.log(data)
+      this.adList = data.content[0].adDTOList
+    }
+  },
+  computed: {
+    activeAdList () {
+      return this.adList.filter(item => item.status === 1)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.my-swipe{
+    width: 100%;
+    .van-swipe-item {
+      overflow: hidden;
+      display: flex;
+      justify-content: center;
+      img{
+        width: 100%;
+      }
+    }
+}
+.course-content-list{
+    top: 230px;
+    bottom: 50px;
+}
+</style>
