92affc5b9e01e87e402c7508d4c352b92e144ba1
GeraltWang <wgeralt@outlook.com>
2021-10-29 23:30:23 +0800

    视频播放制作完成

--- src/views/course-info/video.vue
+++ src/views/course-info/video.vue
@@ -6,6 +6,8 @@
       left-arrow
       @click-left="$router.go(-1)"
     />
+    <!-- 播放组件 -->
+    <div id="video-container"></div>
   </div>
 </template>
 
@@ -38,11 +40,37 @@ export default {
       if (data.state === 1) {
         this.videoInfo = data.content
       }
+      // 初始化播放器
+      const player = new window.Aliplayer({
+        // 播放组件容器的ID
+        id: 'video-container',
+        // 视频ID
+        vid: data.content.fileId,
+        playauth: data.content.playAuth,
+        qualitySort: 'asc',
+        format: 'mp4',
+        mediaType: 'video',
+        width: '100%',
+        height: '100%',
+        autoplay: true,
+        isLive: false,
+        rePlay: false,
+        playsinline: true,
+        preload: true,
+        controlBarVisibility: 'hover',
+        useH5Prism: true
+      }, function (player) {
+        console.log('The player is created')
+      }
+      )
+      console.log(player)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.course-video{
+  height: 210px;
+}
 </style>
